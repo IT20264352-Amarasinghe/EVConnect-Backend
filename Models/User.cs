6c7ef7bb3f8cb621dd818391e4b5c9798fbf73bc
IT20264352-Amarasinghe <it20264352@my.sliit.lk>
2025-09-12 22:41:45 +0530

    make nic primary key of the user table

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,24 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace EVConnectService.Models
 {
     public class User
     {
-        public string NIC { get; set; }   // Primary Key
+        [BsonId]  // This replaces MongoDB's _id with NIC
+        [BsonElement("_id")]
+        public string NIC { get; set; }
+
+        [BsonElement("name")]
         public string Name { get; set; }
+
+         [BsonElement("email")]
         public string Email { get; set; }
+
+        [BsonElement("password")]
         public string Password { get; set; }
+
+        [BsonElement("isActive")]
         public bool IsActive { get; set; } = true;
+
     }
 }
